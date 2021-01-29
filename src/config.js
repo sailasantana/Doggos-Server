a2e061ebbea681de2e97b7344505c9d6fda2a8ea
sailasantana94 <sk4862@nyu.edu>
2021-01-29 18:56:20 -0500

    prep for deploy

--- src/config.js
+++ src/config.js
@@ -2,4 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8040,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://saila@localhost/doggoUser',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://saila@localhost/doggoUser-test'
+
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 
 const app = require('./app')
-
 const knex = require('knex');
 const {PORT, DATABASE_URL} = require('./config');
+
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const db = knex({
   client: 'pg',
