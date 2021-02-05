8f7656d810da57711408ba5950ec8500c87e82c0
sailasantana94 <sk4862@nyu.edu>
2021-02-05 12:21:58 -0500

    test added

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://saila@localhost/doggoUser',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://saila@localhost/doggoUser-test',
-    JWT_SECRET: process.env.JWT_SECRET || 'MY_SECRET_KEY'
+    JWT_SECRET: process.env.JWT_SECRET || 'secret'
 
 }
